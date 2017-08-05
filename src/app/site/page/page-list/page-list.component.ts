1337a60d774bd2c07e6120ed63b6ea4983cd808e
Francis Jones <france100@gmail.com>
2017-08-05 23:22:45 +0100

    Page-list fade transition added

--- src/app/site/page/page-list/page-list.component.ts
+++ src/app/site/page/page-list/page-list.component.ts
@@ -1,11 +1,25 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, state, style, transition, animate, trigger } from '@angular/core';
 import { Router } from "@angular/router";
 import { Page } from "app/models/Page/Page";
 
 @Component({
     selector: 'page-list',
     templateUrl: './page-list.component.html',
-    styleUrls: ['./page-list.component.css']
+    styleUrls: ['./page-list.component.css'],
+    animations: [
+        trigger(
+            "animation",
+            [
+                transition(
+                    ":enter",
+                    [
+                        style({opacity: 0}),
+                        animate(500, style({ opacity: 1 }))
+                    ]
+                )
+            ]
+        )
+    ]
 })
 export class PageListComponent implements OnInit {
 
@@ -16,11 +30,15 @@ export class PageListComponent implements OnInit {
     constructor(private router:Router) { }
 
     ngOnInit() {
+        if (!this.pages) {
+            this.pages = new Array<Page>();
+        }
+
         var urlSegments = this.router.parseUrl(this.router.url).root.children.primary.segments;
 
         if (urlSegments.length > 1) {
             this.activePage = urlSegments[1].path;
-        }        
+        }
     }
 
     goToPage(id:string) {
