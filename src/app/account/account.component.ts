9de7c69d5a1682921c81472de497a39cde99c75a
Francis Jones <france100@gmail.com>
2017-08-07 17:44:24 +0100

    Fixed issue with account navbar routing. Updated logout page text.

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from "rxjs/Subscription";
 import { CommonService } from "app/common.service";
+import { Router } from "@angular/router";
 
 @Component({
     selector: 'app-account',
@@ -10,25 +11,35 @@ import { CommonService } from "app/common.service";
 export class AccountComponent implements OnInit {
 
     public showLogin: boolean;
-    private subscription:Subscription;
+    private accountSubscription:Subscription;
+    private routeSubscription:Subscription;
 
-    constructor(private commonService:CommonService) { }
+    constructor(private router:Router, private commonService:CommonService) { }
 
     ngOnInit() {
         this.showLogin = true;
         
-        this.subscription = this.commonService.accountActionObservable$.subscribe(
+        this.accountSubscription = this.commonService.accountActionObservable$.subscribe(
             result => this.showAccountPage(result)
         );
     }
 
     ngOnDestroy() {
-        if (this.subscription)
-            this.subscription.unsubscribe();
+        if (this.accountSubscription)
+            this.accountSubscription.unsubscribe();
     }
 
     showAccountPage(showLogin:boolean) {
         this.showLogin = showLogin;
+    }
+
+    private handleRouteParams(action:string) {
+        if (action == "register") {
+            this.showLogin = false;
+        }
+        else {
+            this.showLogin = true;
+        }
     }
 
 }
