5d0b97d5b32c99bd8a446fa47decfa2805874ba9
Francis Jones <france100@gmail.com>
2017-09-07 23:01:01 +0100

    Error page has some actual functionality now

--- src/app/error/error.component.ts
+++ src/app/error/error.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { Subscription } from "rxjs/Subscription";
 
 @Component({
-  selector: 'app-error',
-  templateUrl: './error.component.html',
-  styleUrls: ['./error.component.css']
+    selector: 'app-error',
+    templateUrl: './error.component.html',
+    styleUrls: ['./error.component.css']
 })
 export class ErrorComponent implements OnInit {
 
-  constructor() { }
+    private subscription:Subscription;
 
-  ngOnInit() {
-  }
+    public errorCode:string;
+    public errorMessage:string;
+
+    constructor(private route:ActivatedRoute) { }
+
+    ngOnInit() {
+        this.subscription = this.route.params.subscribe(
+            params => this.setErrorMessage(params["code"]),
+            error => this.setErrorMessage("-1")
+        );
+    }
+
+    private setErrorMessage(code:string) {
+        this.errorCode = code;
+        this.errorMessage = this.getMessageFromCode(code);
+    }
+
+    private getMessageFromCode(code:string):string {
+        switch(code) {
+            case "401":
+                return "You are not authorized.";
+            case "404":
+                return "Not found.";
+            default:
+                return "An error occurred.";
+        }
+    }
 
 }
