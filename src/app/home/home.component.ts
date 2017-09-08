6f584d17a5183ed42880470867f45c9405f75f7a
Francis Jones <france100@gmail.com>
2017-09-08 03:58:46 +0100

    Changed colour scheme and tinkered with layout.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, state, trigger, transition, animate, style } from '@angular/core';
 import { LocalStorageValues } from "app/common.values";
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css']
+    styleUrls: ['./home.component.css'],
+    animations: [
+        trigger(
+            "animation",
+            [
+                transition(
+                    ":enter",
+                    [
+                        style({ opacity: 0 }),
+                        animate("500ms", style({ opacity: 1 }))
+                    ]
+                ),
+                transition(
+                    ":leave",
+                    [
+                        style({ opacity: 1 }),
+                        animate("0ms", style({ opacity: 0 }))
+                    ]
+                )
+            ]
+        )
+    ]
 })
 export class HomeComponent implements OnInit {
 
